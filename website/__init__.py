5df88cd013de273c963eb2eba5cbbeb5f5decd63
dien204 <nguyenthimyduyen031200@gmail.com>
2025-03-14 22:35:28 +0700

    modified .gitignore

--- website/__init__.py
+++ website/__init__.py
@@ -10,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'mysecretkey'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['UPLOAD_FOLDER'] = 'static/uploads'
     db.init_app(app)
     
     from .views import views
